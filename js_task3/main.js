12c65fac9bd335701e68b8b90d767cf9b7ddcb87
Vlad <you@example.com>
2021-05-10 23:16:21 +0300

    task_3_edit

--- js_task3/main.js
+++ js_task3/main.js
@@ -4,20 +4,29 @@
 // Например:
 // list("4 5 29 54 4 0 -123 666 -64 1 -3 6 -6") ----> "666 -123"
 
-let list = (str) => {
-  str = str.split(' ');
-  let numMax = str[0];
-  let numMin = str[0];
-  for (i = 1; i < str.length; i++) {
-    if (str[i] > numMax) {
-      numMax = str[i];
-    } if (str[i] < numMin) {
-      numMin = str[i];
-    }
-  } 
+// let list = (str) => {
+//   str = str.split(' ');
+//   let numMax = str[0];
+//   let numMin = str[0];
+//   for (i = 1; i < str.length; i++) {
+//     if (str[i] > numMax) {
+//       numMax = str[i];
+//     } if (str[i] < numMin) {
+//       numMin = str[i];
+//     }
+//   } 
   
- return numMin +' '+ numMax;
+//  return numMin +' '+ numMax;
+// } 
+
+// console.log(list("4 5 29 54 4 0 -123 666 -64 1 -3 6 -6"));
+
+
+
+let list = (str) => {
+  let min = str.split(' ').sort().shift();
+  let max = str.split(' ').sort().pop();
+  return min + ' ' + max;
 } 
 
 console.log(list("4 5 29 54 4 0 -123 666 -64 1 -3 6 -6"));
-
