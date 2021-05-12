a9556e2c583dc8629ee43920acf1c3e7a4b76421
Vlad <you@example.com>
2021-05-12 21:55:04 +0300

    task_21

--- js_task21/main.js
+++ js_task21/main.js
@@ -1,7 +1,29 @@
-// Напишите функцию, которая находит факториал 5.
+// Представьте, что Вы и Ваши друзья решили создать команду мечты. 
+// У этой команды должно быть крутое секретное имя, которое содержит зашифрованную информацию о ней. 
+// Например, это могут быть первые буквы имен его членов в верхнем регистре, отсортированные по алфавиту. 
+// Ваша задача - написать функцию createDreamTeam (members), которая возвращает имя созданной команды (строку) на основе имен ее участников (Array). Удачи!
+// Имена участников должны быть строками. Значения другого типа следует игнорировать. 
+// В случае неправильного типа членов функция должна возвращать false.
+
+// Например:
+
+// createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
+
+// createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
 
 
-let fuck = (n) => {
-    return (n != 1) ? n * fuck(n - 1) : 1;
+let createDreamTeam = str => {
+    return str.filter((item, index, array) => {
+        if (item == Number(item) || item == null || item == Boolean(item)) {
+            return false
+        } else {
+           return (str) = array.map(item => item).sort();
+        }
+});
 }
-console.log(fuck(5))
+console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]))
+
+// let createDreamTeam = str => {
+//     return str.map(item => item[0]).sort().join('');
+// }
+// console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']))
