7bf445b457ece7baa879c125104fd9e4d8fdf6a4
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-05-13 17:57:36 +0300

    task_26

---
+++ js_task26/main.js
@@ -0,0 +1,17 @@
+// Напишите функцию startsWith(), которая определяет, начинается ли строка символами другой строки, 
+// возвращая, соотвественно, true или false.
+
+// let str = 'abc def ghi jkl mno pqr stu';
+
+// String.prototype.startsWith = function(start) {};
+
+// str.startsWith('abc')
+
+let str = 'abc def ghi jkl mno pqr stu';
+String.prototype.startsWith = function(start) {
+   
+};
+
+
+console.log(str.startWish('abc'))
+// console.log(str.startsWith('abc'))
