02bb2b442abc815f9d82143d7ff04ea55701c75d
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-05-14 13:08:30 +0300

    task_38

--- js_task37/main.js
+++ js_task37/main.js
@@ -8,7 +8,6 @@
 let arr = ["php", "php", "css", "css", "script", "script", "html", "html", "java" ];
 
 let removeDuplicates = arr => {
-  return arr.filter((item, index, array) => {return array.indexOf(item) === index;})
-}
+  return arr.filter((item, index, array) => {return array.indexOf(item) === index;})}
 
 console.log((removeDuplicates(arr)));
