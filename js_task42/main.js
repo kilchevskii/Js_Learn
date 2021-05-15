9a500185052f0edd0af5e9b6a7b412f1c8586e19
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-05-15 10:17:23 +0300

    edit_42

--- js_task42/main.js
+++ js_task42/main.js
@@ -4,16 +4,20 @@
 // ["кот", "ток", "кто"] - true
 // ["кот", "тк", "кТо"] - false
 
-function checkAnagram(arr) {
-   if (!arr || arr.length <= 1) return false  
-  let first_word = arr[0].split("").sort().join("");
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i].split("").sort().join("") != first_word) {
-      return false;
-    }
-  }
-  return true;
-}
+// function checkAnagram(arr) {
+//    if (!arr || arr.length <= 1) return false  
+//   let first_word = arr[0].split("").sort().join("");
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i].split("").sort().join("") != first_word) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+
+let checkAnagram = arr => {
+    return arr.filter(item => item.split('').sort().join('').localeCompare(arr[0].split('').sort().join('')) === 0).length === arr.length;}
 
 
 console.log(checkAnagram(["кот", "ток", "кто"]));
