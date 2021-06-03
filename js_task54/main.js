9b3324b0822b7089fa838f4f35d2319cd285d905
Vlad <you@example.com>
2021-06-03 21:12:22 +0300

    task 55

--- js_task54/main.js
+++ js_task54/main.js
@@ -3,20 +3,12 @@
 // Дайте свой ответ в виде строки, соответствующей «нечетному» или «четному».
 
 // Если входной массив пуст, считайте его: [0] (массив с нулем). 
-// Input: [0]
-// Output: "even"
-
-// Input: [0, 1, 4]
-// Output: "odd"
-
-// Input: [0, -1, -5]
-// Output: "even"
+// oddOrEven: [0] => "even"
+// oddOrEven: [0, 1, 4] => "odd"
+// oddOrEven: [0, -1, -5] => "even"
 
 function oddOrEven(arr) {
   let sum = arr.reduce((sum, item) => item + sum, 0)
   return sum % 2 == 0 ? 'even' : 'odd'
-  //   return 'even'
-  // } 
-  //   return 'odd'
 }
 console.log(oddOrEven([0]));
