4c2a8f712ecd5cb1d4bc056b2a2ae50b0f54322d
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-06-04 14:35:03 +0300

    task 63

---
+++ js_task63/main.js
@@ -0,0 +1,14 @@
+// Что такое анаграмма? Что ж, два слова являются анаграммами друг друга, если они оба содержат одинаковые буквы. 
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+
+function anagrams(a, b) {
+  betta = b.join(' ')
+
+
+}
+
+console.log(anagrams('laser', ['lazing', 'lazy',  'lacer']));
+
